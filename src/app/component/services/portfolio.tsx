52ca79f025c39f40525ab6b155eec1d91d2aa4ff
DAVIS ISIBOR <davisisibor@gmail.com>
2023-07-26 11:29:12 +0100

    Update portfolio.tsx

--- src/app/component/services/portfolio.tsx
+++ src/app/component/services/portfolio.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
 const Portfolio = () => {
@@ -8,7 +9,10 @@ const Portfolio = () => {
       <div className="w-full  p_r h-auto py-[80px] flex flex-col gap-[50px]">
         <p className="nova text-[30px] font-[500]">LATEST ON PORFOLIO</p>
         <div className="flex   gap-[30px] justify-between flex-wrap w-full h-auto">
-          <div className="w-[48.5%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]">
+          <Link
+            href="#"
+            className="w-[48%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]"
+          >
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-[10px] capitalize">
                 <p className="port_first_text">finance</p>
@@ -18,8 +22,11 @@ const Portfolio = () => {
                 <i className="bi bi-arrow-up-right"></i>
               </div>
             </div>
-          </div>
-          <div className="w-[23%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]">
+          </Link>
+          <Link
+            href="#"
+            className="w-[22%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]"
+          >
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-[10px] capitalize">
                 <p className="port_first_text">adventure</p>
@@ -29,8 +36,11 @@ const Portfolio = () => {
                 <i className="bi bi-arrow-up-right"></i>
               </div>
             </div>
-          </div>
-          <div className="w-[23%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]">
+          </Link>
+          <Link
+            href="#"
+            className="w-[22%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]"
+          >
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-[10px] capitalize">
                 <p className="port_first_text">beauty</p>
@@ -40,8 +50,11 @@ const Portfolio = () => {
                 <i className="bi bi-arrow-up-right"></i>
               </div>
             </div>
-          </div>
-          <div className="w-[23%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]">
+          </Link>
+          <Link
+            href="#"
+            className="w-[22%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]"
+          >
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-[10px] capitalize">
                 <p className="port_first_text">housing</p>
@@ -51,8 +64,11 @@ const Portfolio = () => {
                 <i className="bi bi-arrow-up-right"></i>
               </div>
             </div>
-          </div>
-          <div className="w-[23%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]">
+          </Link>
+          <Link
+            href="#"
+            className="w-[22%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]"
+          >
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-[10px] capitalize">
                 <p className="port_first_text">portfolio</p>
@@ -62,8 +78,11 @@ const Portfolio = () => {
                 <i className="bi bi-arrow-up-right"></i>
               </div>
             </div>
-          </div>
-          <div className="w-[48.5%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]">
+          </Link>
+          <Link
+            href="#"
+            className="w-[48%]  port_wrap rounded-[10px]  cursor-pointer py-[30px] h-[350px] px-[30px]"
+          >
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-[10px] capitalize">
                 <p className="port_first_text">kids</p>
@@ -73,7 +92,7 @@ const Portfolio = () => {
                 <i className="bi bi-arrow-up-right"></i>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </>
