cf629b5dd6b737fe9ab3372a4e89bdc4f797a9e2
DAVIS ISIBOR <davisisibor@gmail.com>
2023-07-28 19:05:27 +0100

    complete

--- src/app/component/home/home_portfolio.tsx
+++ src/app/component/home/home_portfolio.tsx
@@ -16,14 +16,74 @@ const Home_portfolio = () => {
 
         {/* body of the about section */}
         <div className="w-full flex justify-between h-auto">
-          <div className="h-auto w-[27%]">
-            <div className=" rounded-[12px] h-[350px] border2"></div>
+          <div className="h-auto   flex flex-col  w-[27%]">
+            <div
+              className=" rounded-[12px]  port_gen w-full relative h-[300px] "
+              style={{ backgroundImage: "url(/home/port1.webp)" }}
+            >
+              <div className="pl-[10px] absolute rounded-l-[12px] top-[-5px] right-0 pb-[10px]  pt-[5px] bg-white">
+                <div className="py-[10px] px-[15px] rounded-[12px] bg-black text-white">
+                  {" "}
+                  <i className="bi  bi-arrow-up-right"></i>
+                </div>
+              </div>
+
+              {/* the text */}
+            </div>
+            <div className="w-full  flex flex-col gap-[0px] nova">
+              <p className="text-[18px] text-[grey] capitalize">
+                Social Media - Business
+              </p>
+              <h3 className="text-[32px] font-medium capitalize">
+                Bondbullies
+              </h3>
+            </div>
           </div>
-          <div className="h-auto w-[40%]">
-            <div className="w-full rounded-[12px] h-[350px] border2"></div>
+          <div className="h-auto   flex flex-col  w-[40%]">
+            <div
+              className=" rounded-[12px]  port_gen w-full relative h-[300px] "
+              style={{ backgroundImage: "url(/home/port2.webp)" }}
+            >
+              <div className="pl-[10px] absolute rounded-l-[12px] top-[-5px] right-0 pb-[10px]  pt-[5px] bg-white">
+                <div className="py-[10px] px-[15px] rounded-[12px] bg-black text-white">
+                  {" "}
+                  <i className="bi  bi-arrow-up-right"></i>
+                </div>
+              </div>
+
+              {/* the text */}
+            </div>
+            <div className="w-full  flex flex-col gap-[0px] nova">
+              <p className="text-[18px] text-[grey] capitalize">
+                Adventure - Climbing
+              </p>
+              <h3 className="text-[32px] font-medium capitalize">
+                Climb Onsight
+              </h3>
+            </div>
           </div>
-          <div className="h-auto w-[27%]">
-            <div className="w-full rounded-[12px] h-[350px] border2"></div>
+          <div className="h-auto   flex flex-col  w-[27%]">
+            <div
+              className=" rounded-[12px]  port_gen w-full relative h-[300px] "
+              style={{
+                backgroundImage: "url(/home/port3.webp)",
+              }}
+            >
+              <div className="pl-[10px] absolute rounded-l-[12px] top-[-5px] right-0 pb-[10px]  pt-[5px] bg-white">
+                <div className="py-[10px] px-[15px] rounded-[12px] bg-black text-white">
+                  {" "}
+                  <i className="bi  bi-arrow-up-right"></i>
+                </div>
+              </div>
+
+              {/* the text */}
+            </div>
+            <div className="w-full  flex flex-col gap-[0px] nova">
+              <p className="text-[18px] text-[grey] capitalize">
+                Housing - Construction
+              </p>
+              <h3 className="text-[32px] font-medium capitalize">Atella</h3>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/app/component/home/review.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const Review = () => {
+  return (
+    <>
+      <div className="w-full"></div>
+    </>
+  );
+};
+
+export default Review;
