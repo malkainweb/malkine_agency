79a5bc1530ae7b7bed0cf864695f833184be65e5
DAVIS ISIBOR <davisisibor@gmail.com>
2023-08-16 14:41:58 +0100

    send

--- src/app/component/home/our_aim.tsx
+++ src/app/component/home/our_aim.tsx
@@ -6,11 +6,13 @@ const Aim = () => {
   return (
     <>
       <div className="w-full  h-auto  bg-white ">
-        <div className="w-full h-auto aim p_r rounded-[1.33vw] bg-black">
-          <div className="w-full  flex items-start pt-[4vw]  relative h-[46.6vw]">
+        <div className="w-full h-auto aim p_r rounded-[1.33vw] bg-black sm:mt-[10vw] sm:rounded-none">
+          <div className="w-full  flex items-start pt-[4vw]  relative h-[46.6vw]  sm:h-[92.5vw]">
             <div className="flex flex-col gap-[0.7vw] text-white">
-              <p className="nova text-[1.33vw] text-[#D01717] ">OUR AIM</p>
-              <h3 className="text-[3.33vw] h-fit nova font-bold">
+              <p className="nova text-[1.33vw] text-[#D01717] sm:text-[2.5vw] sm:font-[700]">
+                OUR AIM
+              </p>
+              <h3 className="text-[3.33vw] h-fit nova font-bold sm:hidden">
                 We provide user centered <br />{" "}
                 <span
                   className="underline  underline-offset-4  decoration-[#D01717] "
@@ -22,6 +24,12 @@ const Aim = () => {
                 </span>{" "}
                 and creative designs.
               </h3>
+              <h3 className="text-[6vw] h-fit nova font-bold sm:block hidden">
+                We provide user <br /> centered innovative <br /> and creative
+                designs.
+              </h3>
+
+              <div className="sm:w-[11.25vw] h-[0.55vw] bg-[#D01717] sm:mt-[6vw] hidden sm:block"></div>
             </div>
           </div>
         </div>
