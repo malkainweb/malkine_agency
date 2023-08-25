63c94378d6cd78f6ee7c6fccc4ab9e5e44fb204a
DAVIS ISIBOR <davisisibor@gmail.com>
2023-08-25 04:55:28 +0100

    Update menu.tsx

--- src/app/component/menu.tsx
+++ src/app/component/menu.tsx
@@ -100,9 +100,106 @@ const Menu = (props: any) => {
 
         <motion.div
           animate={{ y: right, backgroundColor: "#0C0C0C", opacity: opacity }}
-          // initial={{ y: "-100%" }}
+          initial={{ y: "-100%" }}
           transition={{ duration: 0.7 }}
-          className="h-[100vh] sm:w-[100vw]  w-[70vw] px-[2.6vw]   py-[5.3vw] sm:px-[3vw] sm:py-[0px] flex justify-center items-center "
+          className="h-[100vh] sm:w-[100vw]  w-[70vw] px-[2.6vw]   py-[5.3vw] sm:px-[3vw] sm:py-[0px] flex justify-center items-center sm:hidden"
+        >
+          <div className="flex justify-between w-full h-full  items-center sm:justify-start sm:flex-col sm:gap-[10vw] sm:items-start">
+            {/* the first flex with the big options */}
+            <div className="hidden sm:flex  sm:w-full sm:h-[100px] sm:justify-between sm:items-center ">
+              <Logo />
+              <div className="hidden  sm:flex sm:justify-center sm:items-center sm:w-[10vw] sm:h-[10vw] sm:bg-opacity-[10%] sm:bg-white sm:rounded-[100%] ">
+                <i
+                  className="bi bi-x-lg  text-[5vw]  font-[700]  cursor-pointer hover:bg-[#B1A2A2] text-white"
+                  onClick={handleCancel}
+                ></i>
+              </div>
+            </div>
+
+            <div className="w-auto h-[40vw] sm:h-[100vw] sm:w-full gap-[30px] flex flex-col justify-between  text-white novabold ">
+              <p className="text-[1.3vw] text-[#9E9E9E] sm:hidden  opacity-0">
+                {" "}
+                i
+              </p>
+              <div className="w-full h-full flex flex-col justify-between    ">
+                {menu_arr.map((e: any, index: any) => {
+                  return (
+                    <>
+                      <div
+                        className="flex items-center gap-[5px] sm:gap-[1.25vw]"
+                        key={index}
+                      >
+                        <Link
+                          href={e.link}
+                          className="novabold capitalize text-[4.6vw] sm:text-[45px] leading-[100%] cursor-pointer hover:text-[#CD6464] transition duration-[1]"
+                          style={{
+                            color: e.link == pathname ? "#D01717" : "",
+                          }}
+                        >
+                          {e.txt}
+                        </Link>
+                        <p className="text-[#9E9E9E] text-[1.3vw] sm:text-[20px]  h-fit">
+                          {e.no}
+                        </p>
+                      </div>
+                    </>
+                  );
+                })}
+                <div className="hidden sm:block w-[80vw] h-[0.5vw] bg-[#DB4F4F]"></div>
+              </div>
+            </div>
+
+            <Link
+              href="/"
+              className="nova hidden sm:block hover:bg-[#CDCCCC] transition-all text-[14px] text-[#4B4B4B] bg-[#F3F3F3] py-[3vw] px-[4vw] rounded-[5vw]"
+            >
+              Become a client <i className="bi bi-arrow-right"></i>
+            </Link>
+
+            {/* the second flex with the small small options */}
+            <div className=" w-fit   sm:hidden h-[40vw] flex  flex-col  justify-between ">
+              <div className="flex gap-[20px] items-center">
+                <Link
+                  href="/"
+                  className="nova  hover:bg-[#CDCCCC] transition-all text-[1.33vw] text-[#4B4B4B] bg-[#F3F3F3] py-[0.75vw] px-[1.3vw] rounded-[3vw]"
+                >
+                  Become a client
+                </Link>
+                <i
+                  className="bi bi-x-lg py-[0.75vw] px-[1.2vw] text-[1.6vw] rounded-[100%] font-[700] bg-[#FFFFFF] bg-opacity-[10%] cursor-pointer hover:bg-[#B1A2A2] text-white"
+                  onClick={handleCancel}
+                ></i>
+              </div>
+
+              <div className="flex w-auto gap-[10px] items-center">
+                <div className="flex flex-col gap-[15px] novabold">
+                  <p className="text-[1.2vw] text-[#FFFFFF]">
+                    Support@rancroft.com <i className="bi bi-clipboard"></i>
+                  </p>
+                  <p className="text-[1.06vw] text-[#9E9E9E]">
+                    Privacy policy & Cookies
+                  </p>
+                  <p className="text-[0.8vw] text-[#9E9E9E]">
+                    <i className="bi bi-c-circle"></i>Rancroft2023
+                  </p>
+                </div>
+
+                {/* the gloabal and exclamation mark
+                 */}
+                <div className="w-auto flex flex-col gap-[10px]">
+                  <i className="bi bi-globe2 py-[5px] px-[10px] text-[1vw] rounded-[100%] font-[700] bg-[#FFFFFF] bg-opacity-[10%] text-white"></i>
+                  <i className="bi bi-exclamation-lg py-[5px] px-[10px] text-[1vw] rounded-[100%] font-[700] bg-[#FFFFFF] bg-opacity-[10%] text-white"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div
+          animate={{ y: right, backgroundColor: "#0C0C0C", opacity: opacity }}
+          initial={{ y: "100%" }}
+          transition={{ duration: 0.7 }}
+          className="h-[100vh] sm:w-[100vw]  w-[70vw] px-[2.6vw]   py-[5.3vw] sm:px-[3vw] sm:py-[0px] hidden justify-center items-center sm:flex "
         >
           <div className="flex justify-between w-full h-full  items-center sm:justify-start sm:flex-col sm:gap-[10vw] sm:items-start">
             {/* the first flex with the big options */}
