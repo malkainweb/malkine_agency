a6f7a0fc43c561b4180dbbc5dc78081b59ac27fe
DAVIS ISIBOR <davisisibor@gmail.com>
2023-08-27 15:17:09 +0100

    send

--- src/app/component/home/team.tsx
+++ src/app/component/home/team.tsx
@@ -1,27 +1,53 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import location from "../../../../public/home/location.webp";
+import { useScroll, useMotionValueEvent, useTransform } from "framer-motion";
 
 const Home_Team = () => {
   const [active, setactive] = useState(1);
+  const width = globalThis.innerWidth / 1.5;
   const [go, setgo] = useState("0vw");
+  const [indicator, setindicator] = useState(0);
 
   const change = (i: any, e: any) => {
     setactive(i);
     setgo(e);
   };
+
+  // get the x value of something
+  const carouselRef = useRef(null);
+  const { scrollX } = useScroll({
+    container: carouselRef,
+  });
+
+  const scroll_lever = useTransform(
+    scrollX,
+    // Map x from these values:
+    [0, width * 3],
+
+    // Into these values:
+    [0, 66],
+  );
+
+  // use motion values to console .log
+  useMotionValueEvent(scroll_lever, "change", (latest) => {
+    console.log(latest);
+    setindicator(latest);
+  });
   return (
     <>
       <div
-        className={`w-full h-auto sm:h-[45vw] sm:block sm:relative hidden sm:overflow-x-hidden `}
+        className={`w-full h-auto sm:h-[45vw] no-scrollbar sm:block sm:relative hidden sm:overflow-x-scroll `}
+        ref={carouselRef}
+        // style={{overflow: '-moz-'}}
       >
         <div
           style={{
             transform: `translateX(${go})`,
           }}
-          className=" transition duration-[1.5s] w-full sm:w-[300vw] bg-white  sm:bg-black sm:absolute sm:top-0  h-auto  p_r  py-[3.3vw] flex justify-between gap-[1.25vw] items-center  sm:gap-[4vw] "
+          className=" transition duration-[1.5s] w-full sm:w-[300vw] bg-white  sm:bg-black sm:absolute sm:top-0  h-auto  pl-[3.5vw] pr-[2.25vw]  py-[3.3vw] flex justify-between gap-[1.25vw] items-center  sm:gap-[4vw] "
         >
           <div className="flex w-full h-[10.3vw] sm:w-[95vw] sm:h-[38vw] sm:rounded-[5.57vw] sm:px-[4vw] sm:py-[3vw] rounded-[2vw] bg-[#F1F1F1] sm:bg-[#0A0A0A] sm:text-white  flex-col p-[1.25vw] justify-between  ">
             <div className="w-full flex justify-between font-[helvetica] ">
@@ -103,15 +129,18 @@ const Home_Team = () => {
         </div>
       </div>
       <div className="w-full  justify-center hidden sm:flex pt-[8vw] pb-[8vw]">
-        <div className="  w-[80vw] h-[2.5vw] bg-[#222222] rounded-[4vw] flex">
+        <div className="  w-[80vw] h-[2.5vw] relative bg-[#222222] rounded-[4vw] flex">
           <div
-            className="w-full h-full cursor-pointer rounded-[1vw]"
+            className="w-[27vw] h-[2.5vw] top-0 absolute cursor-pointer rounded-[1vw]"
             onClick={() => {
               change(1, "-1vw");
             }}
-            style={{ backgroundColor: active != 1 ? "transparent" : "white" }}
+            style={{
+              backgroundColor: active != 1 ? "transparent" : "white",
+              left: `${indicator}%`,
+            }}
           ></div>
-          <div
+          {/* <div
             className="w-full h-full cursor-pointer rounded-[1vw]"
             onClick={() => {
               change(0, "-100vw");
@@ -124,7 +153,7 @@ const Home_Team = () => {
               change(2, "-199vw");
             }}
             style={{ backgroundColor: active != 2 ? "transparent" : "white" }}
-          ></div>
+          ></div> */}
         </div>
       </div>
     </>
