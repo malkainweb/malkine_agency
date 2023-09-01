9b44a834c42ed2ce8ba27fa3c5678f581a310b00
DAVIS ISIBOR <davisisibor@gmail.com>
2023-09-01 13:17:22 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./style.css";
 import "./global_loader.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-NNC83QXFK8"
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NNC83QXFK8', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <body className="hide_now">{children}</body>
     </html>
   );
