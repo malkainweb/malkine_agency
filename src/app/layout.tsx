afaa8ccca4fe647a456992bb4fcd6cc0ace07b70
DAVIS ISIBOR <davisisibor@gmail.com>
2023-09-01 15:34:06 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,9 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-NNC83QXFK8');
+              gtag('config', 'G-NNC83QXFK8' , {
+                 page_path: window.location.pathname,
+              });
             `,
         }}
       />
