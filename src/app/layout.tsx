432c63c58094d656275b404d5398da86a56cf12f
DAVIS ISIBOR <davisisibor@gmail.com>
2023-09-01 20:46:59 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
   return (
     <html lang="en">
       <Script
