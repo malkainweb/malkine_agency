e530962a98bee9131a89ff3623baccdd6cbbb25a
DAVIS ISIBOR <davisisibor@gmail.com>
2023-09-02 05:14:06 +0100

    send

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* goggle trackginig */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} `}
@@ -42,6 +43,20 @@ export default function RootLayout({
               gtag('js', new Date());
               gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
             `,
+        }}
+      />
+
+      {/* bing tracking  */}
+      <Script
+        id="Bing clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", '${process.env.NEXT_PUBLIC_BING_CLARITY_ID}');`,
         }}
       />
 
--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -34,6 +34,20 @@ export default function RootLayout({
             `,
         }}
       />
+
+      {/* bing tracking  */}
+      <Script
+        id="Bing clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", '${process.env.NEXT_PUBLIC_BING_CLARITY_ID}');`,
+        }}
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
