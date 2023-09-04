d75a96fe92a01ffa85fc3fc55882cd73cbf74237
DAVIS ISIBOR <davisisibor@gmail.com>
2023-09-04 22:07:53 +0100

    Update services.tsx

--- src/app/component/home/services.tsx
+++ src/app/component/home/services.tsx
@@ -60,7 +60,7 @@ const Services = () => {
 
   const end_width = new_width + width * 3.5;
 
-  const mob_new_width = width * 12.8;
+  const mob_new_width = width * 12.9;
 
   const mob_end_width = mob_new_width + width * 2;
 
@@ -163,7 +163,7 @@ const Services = () => {
   });
 
   return (
-    <div className="w-full bg-black pb-[2vw] pt-[7vw]  h-[300vw] sm:h-[250vw] p_r flex flex-col sm:gap-[0vw] gap-[2vw] relative ">
+    <div className="w-full bg-black pb-[2vw] pt-[7vw]  h-[300vw] sm:h-[290vw] p_r flex flex-col sm:gap-[0vw] gap-[2vw] relative ">
       {/* first div */}
       <div className="w-full flex sm:flex-col sm:gap-[4vw] sm:items-start justify-between items-center  h-auto text-white">
         <div className="flex flex-col gap-[3vw]  font-[helvetica] ">
@@ -189,7 +189,7 @@ const Services = () => {
       >
         {/* first relative box */}
         <div
-          className="absolute top-0 left-0 h-[13vw] sm:h-[46vh]  w-full  bg-gradient-to-b from-[black] z-[20]"
+          className="absolute top-0 left-0 h-[13vw] sm:h-[46vh]   w-full  bg-gradient-to-b from-[black] z-[20]"
           style={{ overflow: "hidden" }}
         ></div>
         {/* second relative box */}
@@ -202,13 +202,13 @@ const Services = () => {
 
       {/* the normaltext animation */}
       <div
-        className={`w-full h-[19vw] sm:h-[56vw] sm:absolute sm:top-[63vw] sm:left-0  flex flex-wrap justify-start ${des_translate} transition duration-[1s] z-[10]`}
+        className={`w-full h-[19vw] sm:h-[56vw] sm:absolute sm:top-[100vw] sm:left-0  flex flex-wrap justify-start sm:gap-[7vh] ${des_translate} transition duration-[1s] z-[10]`}
       >
         {service_arr.map((e: any, index: any) => {
           return (
             <p
               key={index}
-              className={`w-full text-[4.2vw] sm:text-center sm:text-[4.4vw] sm:font-[600] h-[6vw] sm:h-[10vh]  flex justify-center transition duration-[0.1s] items-center font-[helvetica] text-[${
+              className={`w-full text-[4.2vw] sm:text-center sm:text-[6vw] text-center sm:font-[600] h-[6vw]   flex justify-center transition duration-[0.1s] items-center font-[helvetica] text-[${
                 e.class == check_num ? "#D01717" : "white"
               }] `}
             >
