bf175ed412f74691ab3c6b80e23c50c9ce11c3d2
DAVIS ISIBOR <davisisibor@gmail.com>
2023-09-06 08:15:44 +0100

    Update route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
     from: "onboarding@resend.dev",
     to: ["davisisibor@gmail.com", "jamesmercyn1998@gmail.com"],
     subject: "New Client submission",
-    html: `<h1>Client information from Malkain</h1> <p> Client Name :  <strong > ${"davis"} </strong> </p> <p> Client Email :  <strong > ${"davis@gmail.com"} </strong> </p> <p> Client Phone :  <strong > ${"09087783838"} </strong> </p>  <p> Client Business Name :  <strong > ${"Malkine"} </strong> </p>  <p> Client Project Description :  <strong > ${"i want a good product "} </strong> </p>  <p> Client Is Interested In :  <strong > ${"Ui ux development"} </strong> </p> <p> Client Budget :  <strong > ${"$ 200,99"} </strong> </p> <p> How did you hear about us :  <strong > ${"twieeter"} </strong> </p>  <p style="color:red; ">This information was submitted from  our officail site @copyright 2023</p>  `,
+    html: `<h1>Client information from Malkain</h1> <p> Client Name :  <strong > ${data.name} </strong> </p> <p> Client Email :  <strong > ${data.email} </strong> </p> <p> Client Phone :  <strong > ${data.phone} </strong> </p>  <p> Client Business Name :  <strong > ${data.business} </strong> </p>  <p> Client Project Description :  <strong > ${data.project_info} </strong> </p>  <p> Client Is Interested In :  <strong > ${data.interest} </strong> </p> <p> Client Budget :  <strong > $${data.budget}.00 </strong> </p> <p> How did you hear about us :  <strong > ${data.hear_us} </strong> </p>  <p style="color:red; ">This information was submitted from  our officail site @copyright 2023</p>  `,
     // react: <Logo />,
   });
   return NextResponse.json({ davis: "dkdkd" });
