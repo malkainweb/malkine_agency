5c619186a316b097f379bd5ee6ea3f5bc567c32b
DAVIS ISIBOR <davisisibor@gmail.com>
2023-09-06 09:28:28 +0100

    send

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig = {};
+
+module.exports = nextConfig;
--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -7,14 +7,19 @@ export async function POST(req: Request) {
   // const body = JSON.parse(req.body);
   // const res = await request.json();
   const data = await req.json();
-  console.log(data);
 
   resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: ["davisisibor@gmail.com", "jamesmercyn1998@gmail.com"],
-    subject: "New Client submission",
+    from: `onboarding@resend.dev`,
+    to: [
+      "support@malkain.com",
+      "emmanuel@malkain.com",
+      "malkaindesigns@gmail.com",
+    ],
+    reply_to: data.email,
+    subject: "New Client submission from " + data.name,
     html: `<h1>Client information from Malkain</h1> <p> Client Name :  <strong > ${data.name} </strong> </p> <p> Client Email :  <strong > ${data.email} </strong> </p> <p> Client Phone :  <strong > ${data.phone} </strong> </p>  <p> Client Business Name :  <strong > ${data.business} </strong> </p>  <p> Client Project Description :  <strong > ${data.project_info} </strong> </p>  <p> Client Is Interested In :  <strong > ${data.interest} </strong> </p> <p> Client Budget :  <strong > $${data.budget}.00 </strong> </p> <p> How did you hear about us :  <strong > ${data.hear_us} </strong> </p>  <p style="color:red; ">This information was submitted from  our officail site @copyright 2023</p>  `,
     // react: <Logo />,
   });
+
   return NextResponse.json({ davis: "dkdkd" });
 }
