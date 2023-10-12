f5acbe6a0b5a21a5f9f323d03ef75b0507dd4cc4
DAVIS ISIBOR <davisisibor@gmail.com>
2023-10-12 22:48:02 +0100

    Update contact_showcase.tsx

--- src/app/component/showcase/contact_showcase.tsx
+++ src/app/component/showcase/contact_showcase.tsx
@@ -40,12 +40,12 @@ const Contact_showcase = () => {
       setsending(true);
 
       //    setdisabled(!disabled);
-      window.gtag("event", "form_submission from showcase page", {
+      window.gtag("event", "form_submission ", {
         event_category: "Contact Form",
         event_label: "Submit",
       });
 
-      window.fbq("trackCustom", "submit form from showcase page");
+      window.fbq("trackCustom", "submit form ");
       let data = JSON.stringify({
         name: name,
         phone: phone,
