64e23695cd90e63ffcdf9a51b0c3a22fd53ce338
DAVIS ISIBOR <davisisibor@gmail.com>
2024-05-10 14:03:08 +0100

    update now

--- src/app/[slug]/landing_template.tsx
+++ src/app/[slug]/landing_template.tsx
@@ -1,3 +1,4 @@
+import Calendy from "../component/calendy";
 import Scrolling_hands from "../component/landing_template/scrolling_hand";
 import Writing from "../component/landing_template/writing";
 
@@ -6,6 +7,7 @@ const Landing_template = () => {
     <>
       <Scrolling_hands />
       <Writing />
+      <Calendy />
       <div className="h-[100vw]"></div>
     </>
   );
---
+++ src/app/component/calendy.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React from "react";
+import { InlineWidget, useCalendlyEventListener } from "react-calendly";
+
+const Calendy = () => {
+  useCalendlyEventListener({
+    // onProfilePageViewed: () => console.log("onProfilePageViewed"),
+    onDateAndTimeSelected: () => {
+      // console.log("onDateAndTimeSelected");
+      // Track a "Lead" event with the Facebook Pixel
+      window.fbq("track", "Calendy", {
+        value: "Calendy Date and time selected", // the value of the lead (optional)
+      });
+      window.gtag("event", "Calendy ", {
+        event_category: "Calendy",
+        event_label: "Calendy Date and time selected",
+      });
+      console.log("done done done");
+    },
+    onEventTypeViewed: () => console.log("onEventTypeViewed"),
+    onEventScheduled: (e) => {
+      // console.log(e.data.payload);
+      window.fbq("track", "Calendy success", {
+        value: "Calendy booking successfull", // the value of the lead (optional)
+      });
+      window.gtag("event", "Calendy success", {
+        event_category: "Calendy",
+        event_label: "Calendy booking successfull",
+      });
+    },
+  });
+  return (
+    <div className="w-full flex flex-col items-center my-[5vw] sm:my-[4vw] sm:gap-[6vw] gap-[2vw] ">
+      <h3 className="font_helvetica text-white text-[5vw] sm:text-[7vw] text-center">
+        Let's Get Started
+      </h3>
+      <div className="Calendy w-full    sm:w-[95%]">
+        <InlineWidget
+          url="https://calendly.com/emmanuel-malkaindesigns/malkainhealth"
+          styles={{
+            height: "1000px",
+            width: "100%",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Calendy;
