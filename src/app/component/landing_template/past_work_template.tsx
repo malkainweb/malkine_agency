39d1d879565c7d463f85ef57cc44536dc5acfb46
DAVIS ISIBOR <davisisibor@gmail.com>
2024-05-14 01:24:34 +0100

    update

--- src/app/component/landing_template/past_work_template.tsx
+++ src/app/component/landing_template/past_work_template.tsx
@@ -1,13 +1,41 @@
 "use client";
 import Image from "next/image";
-import img from "../../../../public/landingpage_template/port2.webp";
+import img2 from "../../../../public/landingpage_template/port2.webp";
+import img1 from "../../../../public/landingpage_template/port1.webp";
 import { Typewriter } from "react-simple-typewriter";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 
 // import Typewriter from "typewriter-effect";
 const Past_work_template = () => {
-  const info_array = ["", "", "", "", "", ""];
+  //   const info_array = ["", "", "", "", "", ""];
+
+  const data_array = [
+    {
+      img: img2,
+      text: "HEALTH-ADE - WE HELPED HEALTH-ADE FOLLOW THEIR GUT",
+    },
+    {
+      img: img1,
+      text: "SMARTSWEETS - EXPANDING THE CANDY UNIVERSE WITH SMARTSWEETS.",
+    },
+    {
+      img: img1,
+      text: "NYLON - REDESIGNING AN ICONIC MAGAZINE FOR THE INTERNET AGE.",
+    },
+    {
+      img: img2,
+      text: "HEALTH-ADE - WE HELPED HEALTH-ADE FOLLOW THEIR GUT",
+    },
+    {
+      img: img2,
+      text: "MAEV - THE WELLNESS BRAND FOR CITY DOGS AND THEIR HUMANS.",
+    },
+    {
+      img: img1,
+      text: "PETFOLK - RESHAPING PET CARE FOR TODAY",
+    },
+  ];
 
   const itemsRefs = useRef<any>([]);
   const [visibleIndexes, setVisibleIndexes] = useState<any>([]);
@@ -28,7 +56,7 @@ const Past_work_template = () => {
           }
         });
       },
-      { threshold: 0.6 },
+      { threshold: 0.63 },
     );
 
     itemsRefs.current.forEach((ref: any) => {
@@ -36,11 +64,13 @@ const Past_work_template = () => {
     });
 
     return () => {
-      itemsRefs.current.forEach((ref: any) => {
-        if (ref) {
-          observer.unobserve(ref);
-        }
-      });
+      if (itemsRefs) {
+        itemsRefs.current.forEach((ref: any) => {
+          if (ref) {
+            observer.unobserve(ref);
+          }
+        });
+      }
     };
   }, []);
 
@@ -55,24 +85,22 @@ const Past_work_template = () => {
           We Don{"'"}t Just Talk The Talk <br /> We Walk The Walk{" "}
         </h3>
         <div className="w-full flex px-[1vw] sm:gap-[5vw] sm:px-[3vw] justify-center gap-[3vw] flex-wrap ">
-          {info_array.map((e: any, index: any) => {
+          {data_array.map((e: any, index: any) => {
             return (
               <Link
                 href={"/"}
                 data-index={index}
                 key={index}
                 ref={(ref) => (itemsRefs.current[index] = ref)}
-                className="w-[45%] gap-[0.7vw] translate-y-[10%] opacity-0 transition duration-[1s] sm:gap-[2vw] sm:w-full  flex flex-col "
+                className="w-[45%] mb-[-1vw] gap-[0.7vw] translate-y-[10%] opacity-0 transition duration-[1s] sm:gap-[2vw] sm:w-full  flex flex-col "
               >
-                <div className="w-full text-white h-[25vw] bg-white sm:h-[50vw] overflow-hidden sm:rounded-[4.5vw]  rounded-[2.1vw]">
-                  <Image src={img} alt="img" className="w-full h-fit" />
+                <div className="w-full text-white h-[24vw] bg-[#ffffff71] sm:h-[50vw] overflow-hidden sm:rounded-[4.7vw]  rounded-[2.5vw]">
+                  <Image src={e.img} alt="img" className="w-full h-fit" />
                 </div>
                 <p className="text-white sm:text-[2.5vw] sm:leading-[3vw] text-[0.9vw] font_helvetica">
                   {visibleIndexes.includes(index) && (
                     <Typewriter
-                      words={[
-                        "VII THE SEVENTH ESTATE - REVOLUTIONIZING WINE WITH CARMELO ANTHONY",
-                      ]}
+                      words={[e.text]}
                       cursorStyle=""
                       typeSpeed={30}
                       delaySpeed={1000}
