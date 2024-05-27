c0532dcc242c4017d96855eddf9a21dc97708ec8
DAVIS ISIBOR <davisisibor@gmail.com>
2024-05-27 02:58:21 +0100

    Update layout.tsx

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '450622250894935');
+            fbq('init', ${process.env.NEXT_PUBLIC_META_PIXED_ID});
              fbq('track', 'PageView');
           `,
         }}
