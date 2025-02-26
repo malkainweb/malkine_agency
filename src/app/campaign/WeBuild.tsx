24d4a4cc271fc28b84009d49e625a8a10a6c31dd
Davis Isibor <davisisibor@gmail.com>
2025-02-26 15:33:17 +0100

    update

--- src/app/campaign/WeBuild.tsx
+++ src/app/campaign/WeBuild.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { useMotionValueEvent, useScroll, useTransform } from "framer-motion";
+import {
+  useMotionValueEvent,
+  useScroll,
+  motion,
+  useTransform,
+} from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import UspBackground from "@/../public/campaign/UspAnimation/UspBackground.webp";
 import small_logo from "../../../public/new_options/small_logo.webp";
 import { gsap } from "gsap";
 import { NeueHaasDisplay_bold, NeueHaasDisplay_roman } from "../utils/fonts";
+import WeBuildCards from "./WeBuildCards";
 
 const WeBuild = () => {
   // the array function
@@ -44,87 +50,19 @@ const WeBuild = () => {
   const sectionRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: sectionRef,
-    // offset: ["end start", "end 50%"],
-  });
-  const width = globalThis.innerWidth;
-  useEffect(() => {
-    setcalwidth(width);
-  }, [width]);
-  const [calwidth, setcalwidth] = useState(0);
-  const [yvalue, setyvalue] = useState(1);
-  const [scale_y2value, setscale_y2value] = useState(0);
-  const [height, setheight] = useState(1);
-
-  const y = useTransform(scrollYProgress, [0, 1], [1, items.length + 0.5]);
-  const scale_y = useTransform(scrollYProgress, [0, 1], [10, 1]);
-  const parent_height = useTransform(scrollYProgress, [0, 1], [1, 10]);
-
-  useMotionValueEvent(y, "change", (latest) => {
-    setyvalue(latest);
-    // console.log(latest);
-  });
-  useMotionValueEvent(scale_y, "change", (latest) => {
-    setscale_y2value(latest);
-  });
-  useMotionValueEvent(parent_height, "change", (latest) => {
-    setheight(latest);
+    // offset: ["10% 100%", "100% 100%"], // Start when 30% enters, end when 10% leaves
   });
 
-  // Dynamically creating an array with the same length as `items`
-
-  // this logic is what achieves styling on here
-  const dynamicArray_scaling = Array(items.length)
-    .fill("")
-    .map((_, index) => index);
-
-  const calculateScale = (index: number, data_array: any[], yvalue: number) => {
-    const minScale = Math.min(...data_array) / 2 + scale_y2value / 8;
-    const maxScale = 1;
-    const scaleFactor = (yvalue - (index + 1)) / 6; // adjust this value to control the scaling speed
-    const scale = maxScale - (maxScale - minScale) * scaleFactor;
-
-    return scale;
-  };
+  const progressPercentage = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["50%", "-50%"]
+  );
 
   const itemRefs = useRef<any>([]);
 
   // this is for using gsap
-  useEffect(() => {
-    itemRefs.current.forEach((ref: any, index: any) => {
-      gsap.to(ref, {
-        opacity:
-          index + 1 - yvalue >= 0 && index + 1 - yvalue <= 1
-            ? 1
-            : index + 1 - yvalue <= 0
-            ? 1
-            : 0,
-        transform:
-          index + 1 - yvalue >= 0 && index + 1 - yvalue <= 1
-            ? `translateY(${
-                (index + 2 - yvalue) * 100 - 150 * (-index + yvalue)
-              }%) translateX(-50%)`
-            : index + 1 - yvalue <= 0
-            ? `translateY(${
-                -50 -
-                (yvalue - (index + 1)) * (calwidth < 765 ? 15 / 1.1 : 15 / 1.2)
-              }%) translateX(-50%)  scale(${calculateScale(
-                index,
-                dynamicArray_scaling,
-                yvalue
-              )})
-`
-            : `translateY(${yvalue + 1 + index * 100}%) translateX(-50%)`,
-
-        filter:
-          index + 1 - yvalue >= 0 && index + 1 - yvalue <= 1
-            ? ``
-            : index + 1 - yvalue <= -0.8
-            ? `blur(${(yvalue - index) * 1.2}px)`
-            : ``,
-        duration: 0.4, // Adjust duration as needed
-      });
-    });
-  }, [yvalue]);
+  useEffect(() => {}, []);
   return (
     <>
       <h2
@@ -135,29 +73,36 @@ const WeBuild = () => {
       </h2>
       {/* the wrapper */}
       <div
-        className={`w-full  md:w-[100rem] md:max-w-full mx-auto flex items-end  relative`}
-        style={{ height: `${items.length * 100}vh` }}
+        className={`w-full  md:w-[100rem]  bg-black  md:max-w-full mx-auto flex items-end min-h-[100vh] relative  flex-col`}
+        // style={{ height: `${items.length * 30}vh` }}
         ref={sectionRef}
       >
-        <div className="flex justify-center items-center  overflow-hidden  sticky bottom-0 h-[100vh]  w-full ">
-          {/* the customized scroll bar ends */}
-          {items.map((e, index) => {
-            return (
-              <div
-                key={index}
-                ref={(el: any) => (itemRefs.current[index] = el)}
-                className={`  absolute top-[50%] translate-x-[-50%] rounded-[30px] left-[50%] w-[50%]  sm:w-[90%] md:h-[30rem] md:gap-0 gap-[2rem] p-[3rem] sm:p-[10%] ${
-                  index % 2
-                    ? "md:flex-row-reverse flex-col-reverse"
-                    : "md:flex-row flex-col"
-                }  flex rounded-[16px] bg-black/40  sm:bg-black/20 backdrop-blur-2xl text-white`}
-              >
-                <p className={`${NeueHaasDisplay_roman.className} text-2xl`}>
-                  {e.des}
-                </p>
-              </div>
-            );
-          })}
+        <div className="sticky top-0 left-0 w-full h-[40vh] z-[20] bg-gradient-to-b from-black via-[#000000ca]"></div>
+        <div className="sticky bottom-0 left-0 w-full h-[40vh] z-[20] bg-gradient-to-t from-black via-[#000000ca]"></div>
+
+        <div className="flex justify-center pb-[50vh] items-center  overflow-hidden flex-col  bottom-0 h-full bg-black  w-full ">
+          <motion.div
+            style={
+              {
+                // translateY: progressPercentage,
+                // transition: "0.5s ease-out",
+              }
+            }
+            className=" gap-[6rem] h-fit flex flex-col  w-full"
+          >
+            {/* the customized scroll bar ends */}
+            {items.map((e, index) => {
+              return (
+                <div key={index}>
+                  <WeBuildCards
+                    arr_ref={itemRefs.current[index]}
+                    index={index}
+                    data={e}
+                  />
+                </div>
+              );
+            })}
+          </motion.div>
         </div>
       </div>
     </>
--- src/app/campaign/WeBuildCards.tsx
+++ src/app/campaign/WeBuildCards.tsx
@@ -1,7 +1,59 @@
 "use client";
 
-const WeBuildCards = () => {
-  return <div className="border2"></div>;
+import {
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+  motion,
+} from "framer-motion";
+import Image from "next/image";
+import { gsap } from "gsap";
+import { useEffect, useRef, useState } from "react";
+import { NeueHaasDisplay_bold, NeueHaasDisplay_roman } from "../utils/fonts";
+
+const WeBuildCards = ({
+  data,
+
+  yvalue,
+}: any) => {
+  const ref = useRef(null);
+  const text_ref = useRef(null);
+  const [translate_move, settranslate_move] = useState(10);
+
+  // Get scroll progress for the element
+  const { scrollYProgress } = useScroll({
+    target: ref, // Target the specific element
+    offset: ["50% 100%", "100% 0%"], // Start when 30% enters, end when 10% leaves
+  });
+
+  // Map scroll progress to a percentage value
+  const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.9, 1.4, 0.9]);
+  const fontsize = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    ["1.85rem", "1.9rem", "1.85rem"]
+  );
+
+  return (
+    <div ref={ref} className={`  w-full px-[3%] `}>
+      {/* text that would be animated */}
+      <motion.div
+        ref={text_ref}
+        // style={{ opacity , }}
+        className={` ${NeueHaasDisplay_bold.className}  capitalize flex-shrink-0 `}
+      >
+        {" "}
+        <motion.p
+          style={{ fontSize: fontsize, opacity, transition: "0.5s ease-out" }}
+          className=" text-center text-white"
+        >
+          {data.des}
+        </motion.p>
+      </motion.div>
+    </div>
+  );
 };
 
 export default WeBuildCards;
