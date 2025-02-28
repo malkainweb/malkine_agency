63e8b5fb6517b0821ac08b4dbed33092f3fa6699
Davis Isibor <davisisibor@gmail.com>
2025-02-28 10:53:28 +0100

    Update footer_hand.tsx

--- src/app/component/landing_template/footer_hand.tsx
+++ src/app/component/landing_template/footer_hand.tsx
@@ -33,7 +33,7 @@ const Footer_hand = ({
             }}
             className={`font_helvetica text-white text-[5vw] sm:text-[10vw] text-center ${NeueHaasDisplay_light.className}`}
           >
-            Let's Work together
+            Let{"'"}s Work together
           </h3>
 
           {/* the button */}
