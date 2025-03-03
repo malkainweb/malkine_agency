c061594db6eba5590f5b6bb62f02360a4ee15774
Davis Isibor <davisisibor@gmail.com>
2025-03-03 04:28:40 +0100

    Update WeBuild.tsx

--- src/app/campaign/WeBuild.tsx
+++ src/app/campaign/WeBuild.tsx
@@ -1,91 +1,127 @@
 "use client";
-
-import {
-  useMotionValueEvent,
-  useScroll,
-  motion,
-  useTransform,
-} from "framer-motion";
-import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import UspBackground from "@/../public/campaign/UspAnimation/UspBackground.webp";
-import small_logo from "../../../public/new_options/small_logo.webp";
-import { gsap } from "gsap";
-import { NeueHaasDisplay_bold, NeueHaasDisplay_roman } from "../utils/fonts";
-import WeBuildCards from "./WeBuildCards";
+import copyPaste from "@/../public/campaign/MalkainIsForYouIf/CopyAndPaste.webp";
+import DontWantBeg from "@/../public/campaign/MalkainIsForYouIf/DontWantBeg.webp";
+import bgLine from "@/../public/campaign/MalkainIsForYouIf/bgLine.png";
+import DontWantLeftBehind from "@/../public/campaign/MalkainIsForYouIf/DontWantLeftBehind.webp";
 
-const WeBuild = () => {
-  // the array function
-  const [items, setitems] = useState([
+import { scrollTo_calendy } from "../utils/scroll_to_calendy";
+import {
+  NeueHaasDisplay_bold,
+  NeueHaasDisplay_medium,
+  NeueHaasDisplay_roman,
+} from "../utils/fonts";
+
+const MalkainIsForYouIf = () => {
+  const items = [
     {
-      des: "Websites",
+      heading: "Websites",
+      img: copyPaste,
     },
     {
-      des: "Landing Pages",
+      heading: "landing pages",
+      img: DontWantBeg,
     },
+
     {
-      des: "Custom Apps",
+      heading: "Custom Apps",
+      img: DontWantLeftBehind,
     },
-  ]);
-
-  const sectionRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: sectionRef,
-    offset: ["10% 100%", "100% 100%"], // Start when 30% enters, end when 10% leaves
-  });
-
-  const progressPercentage = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["80%", "-10%"]
-  );
-
-  const itemRefs = useRef<any>([]);
-
-  // this is for using gsap
-  useEffect(() => {}, []);
+  ];
   return (
     <>
-      {/* the wrapper */}
-      <div
-        className={`w-full  md:w-[100rem] z-[10000]  bg-black  md:max-w-full mx-auto   h-[200vh] relative `}
-        // style={{ height: `${items.length * 30}vh` }}
-        ref={sectionRef}
-      >
-        {" "}
+      <div className="w-full flex   flex-col items-center mb-[4vw] sm:gap-[5vw] sm:my-[15vw] ">
         <h2
-          className={`pt-[4rem] pb-[2rem] text-white sticky top-0 z-[100] text-center ${NeueHaasDisplay_bold.className} text-4xl`}
+          className={`pt-[3rem] pb-[2rem] text-center ${NeueHaasDisplay_bold.className} text-4xl`}
         >
-          {" "}
-          We BUILD{" "}
+          We BUILD
         </h2>
-        <div className="flex justify-center items-center h-[100vh] bg-black overflow-hidden flex-coln  top-0  sticky   w-full ">
-          <div className="absolute top-0 left-0 w-full h-[45vh]  z-[20] bg-gradient-to-b  from-black via-[black]"></div>
-          <motion.div
-            style={{
-              translateY: progressPercentage,
-              // transition: "0.5s ease-out",
-            }}
-            className=" gap-[2rem] h-fit flex flex-col  w-full"
-          >
-            {/* the customized scroll bar ends */}
-            {items.map((e, index) => {
-              return (
-                <div key={index} className="">
-                  <WeBuildCards
-                    arr_ref={itemRefs.current[index]}
-                    index={index}
-                    data={e}
-                  />
+
+        {/* the bottom process and the mapping function */}
+        <div className=" w-[70vw] sm:w-full sm:pl-[7vw] sm:pr-[3vw]   flex flex-col items-center justify-between  gap-[12vw] relative">
+          <Image
+            src={bgLine}
+            alt="line"
+            className="w-[4.2vw]  h-full  hidden sm:block absolute left-[1.5vw]"
+          />
+          {items.map((e: any, index: any) => {
+            return (
+              <div
+                key={index}
+                // style={{
+                //   marginTop:
+                //     window && window.innerWidth > 650 ? e.top : e.mobile_top,
+                // }}
+                className={` sm:h-auto   ${e.top ? `mt-[${e.top}] ` : ""} ${
+                  e.mobile_top ? `sm:mt-[${e.mobile_top}]` : ""
+                }  gap-[7vw] sm:gap-[5vw]  sm:flex-col sm:w-full  ${
+                  e.center
+                    ? "w-[26vw] h-[20vw]  sm:w-[96vw] sm:h-auto sm:pt-[14vw] sm:pb-[7vw] sm:px-[4vw] sm:rounded-[7vw] sm:translate-x-[-3%]  bg-[#F8F8F8] rounded-[2vw] items-center z-[99] px-[2vw]"
+                    : "w-full h-auto items-start"
+                }    flex justify-center  ${
+                  e.reverse ? "flex-row-reverse" : "flex-row"
+                }`}
+              >
+                {e.img && (
+                  <div
+                    className={`w-[50%]  rounded-[20px] overflow-hidden sm:w-full  ${
+                      e.reverse ? "justify-start" : "justify-end"
+                    }  flex  `}
+                  >
+                    <Image
+                      src={e.img}
+                      alt={e.heading}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                )}
+
+                {/* the body  */}
+                <div
+                  className={`  sm:gap-[1rem]  sm:items-start  ${
+                    e.center
+                      ? "w-full b items-center text-center "
+                      : "w-[50%] sm:w-full"
+                  } flex flex-col gap-[0.7vw] ${
+                    e.reverse ? "items-end" : "items-start"
+                  }  `}
+                >
+                  {/* <div className="flex justify-center items-center w-[11.2vw] sm:w-[40vw] sm:h-[7.9vw] sm:rounded-[10vw]   h-[2.2vw] border-[0.07vw] border-black rounded-[2.9vw]">
+                    <p
+                      className={`${NeueHaasDisplay_roman.className} text-[0.9vw] sm:text-[3.2vw] uppercase`}
+                    >
+                      {e.step_text}
+                    </p>
+                  </div> */}
+                  <h2
+                    className={`${NeueHaasDisplay_medium.className} ${
+                      e.reverse ? "text-end" : ""
+                    }  text-3xl uppercase text-center  w-full`}
+                  >
+                    {e.heading}
+                  </h2>
+                  {/* 
+                  <p
+                    className={`${
+                      NeueHaasDisplay_roman.className
+                    } text-lg opacity-50 sm:p-0 sm:text-start  ${
+                      e.center == true
+                        ? "p-0"
+                        : e.reverse
+                        ? "text-end pl-[6vw]"
+                        : "pr-[6rem]"
+                    }  `}
+                  >
+                    {e.body}
+                  </p> */}
                 </div>
-              );
-            })}
-          </motion.div>
-          <div className="absolute bottom-0 left-0 w-full h-[45vh]  z-[20] bg-gradient-to-t from-black via-[black]"></div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
   );
 };
 
-export default WeBuild;
+export default MalkainIsForYouIf;
